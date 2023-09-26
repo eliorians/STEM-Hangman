6f8b7398206410e409df0bf8902c28d2e539f737
Aaron White <aaronwhite@Aarons-MacBook-Pro.local>
2023-09-26 18:15:21 -0400

    added difficulty options and new words

--- Hangman.py/hangman.py
+++ Hangman.py/hangman.py
@@ -1,24 +1,35 @@
 
 #sets answer to hangman game
-answer = "test"
+word_choices= ["code", "giraffe", "tashakkori"]
+difficulty = int(input ("choose a difficulty: 0 for easy, 1 medium, 2 for hard: "))
+if difficulty ==0 or difficulty ==1 or difficulty ==2:
+    answer= word_choices [difficulty]
+else:
+    print ("Invalid input, hard choice selected :)") 
+    answer= word_choices[2]
 #prints a _ for each letter in answer
 for i in answer:
     print("_", end=" ")
 print("\n")
-answer = "test"
+
 
 def main():
     tries = 0
+    maxtries=6
     letter_list = []
-    while(tries <= 5):
-        guess = input("Choose a letter: ")
+    while(tries != maxtries):
+        guess = input("Guess a letter: ")
+        guess= guess.lower()
 
-        if guess.isalpha():
+        if guess.isalpha() and len(guess) == 1:
             if guess in answer:
                 print("Nice Job!")
             else:
                 tries = tries + 1
-                print("incorrect")
+                if tries != maxtries:  
+                    print("Incorrect, you have used " + str(tries)+ " out of " + str(maxtries)+" tries. ") 
+                else:
+                    print ("Game Over!") 
         else: 
             print("Invalid input, Try Again :")
        #list of guessed letters
@@ -34,8 +45,7 @@ def main():
         if all(item in letter_list for item in answer):
             print("YOU WIN!!!")
             break #end the game if the player wins
-    print("GAME OVER!",
-          "Play Again!")
+    
 
             
    
