ff25759cb65d817b6379ce5428d7315111e7a5d5
Eli Orians <eli.orians@gmail.com>
2023-09-26 19:00:26 -0400

    cleaning up

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # STEM-Hangman
 Working with Carson Fouts, Aaron White, and CJ Williams to create a project for our STEM Scholarship class. Using this hangman game to show them fundamentals of using VS Code, Git, Python.
 
+To run: 
+.\hangman.py
+
 steps to push to git:
 git status
 git add .
--- hangman.py
+++ hangman.py
@@ -1,46 +1,61 @@
 from DrT import *
-#sets answer to hangman game
-word_choices= ["code", "giraffe", "tashakkori"]
-difficulty = int(input ("choose a difficulty: 0 for easy, 1 medium, 2 for hard: "))
-if difficulty ==0 or difficulty ==1 or difficulty ==2:
-    answer= word_choices [difficulty]
-else:
-    print ("Invalid input, hard choice selected :)") 
-    answer= word_choices[2]
-#prints a _ for each letter in answer
-for i in answer:
-    print("_", end=" ")
-print("\n")
-
 
 def main():
     tries = 0
     maxtries=6
     letter_list = []
+
+    #select word / game difficulty
+    word_choices= ["code", "giraffe", "tashakkori"]
+    difficulty = int(input ("choose a difficulty: 0 for easy, 1 medium, 2 for hard: "))
+
+    if difficulty ==0 or difficulty ==1 or difficulty ==2:
+        answer= word_choices [difficulty]
+    else:
+        print ("Invalid input, hard choice selected :)") 
+        answer= word_choices[2] 
+
+    #show number of characters in word
+    for i in answer:
+        print("_", end=" ")
+    print("\n")
+
+    #main game loop
     while(tries != maxtries):
+
+        #get letter
         guess = input("Guess a letter: ")
+        #force to lower to allow capital letter inputs
         guess= guess.lower()
 
+        #check valid inputs
         if guess.isalpha() and len(guess) == 1:
+
+            #correct answers
             if guess in answer:
                 print("Nice Job!")
+            #incorrect answers
             else:
                 tries = tries + 1
                 if tries != maxtries:  
                     print("Incorrect, you have used " + str(tries)+ " out of " + str(maxtries)+" tries. ") 
                 else:
                     print ("Game Over!") 
+        #invalid inputs
         else: 
-            print("Invalid input, Try Again :")
-       #list of guessed letters
+            print("Invalid input, Try Again:")
+
+        #keep track of guessed letters
         letter_list.append(guess)
+
+        #show location of correct letters
         for i in answer:
             if i in letter_list:
                 print (i, end=" ")
             else:
                 print("_", end=" ")
-
         print ("\n")
+
         #check if the player has guessed all the letters
         if all(item in letter_list for item in answer):
             print("YOU WIN!!!")
@@ -48,11 +63,5 @@ def main():
                 print (drT)
             break #end the game if the player wins
             
-    
-
-            
-   
-
-
 if __name__ == "__main__":
     main()
